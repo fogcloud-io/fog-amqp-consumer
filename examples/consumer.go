dcd0b5e69a7829cac65cb26bea7e06630469a256
zhw <eternalzhw@gmail.com>
2023-01-13 15:26:25 +0800

    style: 💄 examples

--- examples/consumer.go
+++ examples/consumer.go
@@ -2,31 +2,27 @@ package main
 
 import (
 	"log"
-	"time"
 
 	consumer "github.com/fogcloud-io/fog-amqp-consumer"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 var (
-	host     = "localhost"
-	port     = "5672"
-	key      = "xgHc40bf04fb020c"
-	secret   = "c3bad348bb34390558f7f1aacce17877"
-	clientID = "fog-consumer"
+	AMQPHost     = "localhost"
+	AMQPPort     = "5672"
+	AMQPTLS      = false
+	accessKey    = "xgHc40bf04fb020c"
+	accessSecret = "c3bad348bb34390558f7f1aacce17877"
+	clientID     = "fog-consumer"
 )
 
 func main() {
-	cli, err := consumer.NewFogConsumerClient(host, port, key, secret, clientID, false)
+	cli, err := consumer.NewFogConsumerClient(AMQPHost, AMQPPort, accessKey, accessSecret, clientID, AMQPTLS)
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func() {
-		time.Sleep(time.Second * 3)
-		cli.Close()
-	}()
 
-	err = cli.ConsumeWithHandler(100, key, "fog-test", true, false, false, false, nil, func(b amqp.Delivery) { log.Printf("amqp receive: %s", b.Body) })
+	err = cli.ConsumeWithHandler(100, accessKey, "fog-test", false, false, false, false, nil, func(b amqp.Delivery) { log.Printf("amqp receive: %s", b.Body); b.Ack(true) })
 	if err != nil {
 		log.Print(err)
 	}
